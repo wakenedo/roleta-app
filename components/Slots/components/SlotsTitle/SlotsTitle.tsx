c4def8db0bbb262977799eb41f9eb382d14d9e69
wakenedo <a_nuner@outlook.com>
2026-02-03 16:05:24 -0300

    style(SlotsTitle): Add text-shadow

--- components/Slots/components/SlotsTitle/SlotsTitle.tsx
+++ components/Slots/components/SlotsTitle/SlotsTitle.tsx
@@ -1,6 +1,6 @@
 const SlotsTitle = () => {
   return (
-    <h2 className="md:text-4xl text-xl font-bold mb-4 mt-2 ml-8 text-slate-100">
+    <h2 className="md:text-4xl text-xl text-shadow-2xs font-bold mb-4 mt-2 ml-8 text-slate-100">
       Roleta de Ofertas
     </h2>
   );
