73f4f1117305e04742e729b8ac4440a4e3b8c6a2
wakenedo <a_nuner@outlook.com>
2026-02-05 16:13:19 -0300

    feat(UserAreaInterface): New structure and width max size for md displays

--- components/UserAreaInterface/UserAreaInterface.tsx
+++ components/UserAreaInterface/UserAreaInterface.tsx
@@ -17,9 +17,9 @@ const UserAreaInterface = ({
       {/* Overlay */}
       <div className="absolute inset-0 bg-black/50 z-[-1]" />
 
-      <main className="relative z-10 min-h-screen flex flex-col items-center pt-6 px-4">
+      <main className="md:max-w-3xl mx-auto relative z-10 min-h-screen flex flex-col items-center pt-6 px-4">
         {/* Promo */}
-        <div className="w-full max-w-md mb-6">
+        <div className="w-full  mb-6">
           <BannerPromo />
         </div>
 
@@ -27,10 +27,10 @@ const UserAreaInterface = ({
 
         {/* Stats */}
         {user && (
-          <div className="w-full max-w-md grid gap-4 mb-6">
+          <div className="w-full  grid gap-4 mb-6">
             <UserCard user={user} logout={logout} />
+            <UserStats user={user} />
             <DailyQuota />
-            <UserStats />
             <Rewards />
           </div>
         )}
