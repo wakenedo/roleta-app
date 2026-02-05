ce44feebaecd7ade1af35b29af247060a99ed9fa
wakenedo <a_nuner@outlook.com>
2026-02-05 16:10:40 -0300

    feat(UserCard): Added greetings section , might turn out to be a simple placeholder for a future component

--- components/UserAreaInterface/UserCard/UserCard.tsx
+++ components/UserAreaInterface/UserCard/UserCard.tsx
@@ -9,29 +9,37 @@ const UserCard = ({
   logout: () => void;
 }) => {
   return (
-    <div className="w-full max-w-md bg-white/90 backdrop-blur rounded-lg shadow-md p-4 ">
+    <div className="w-full bg-white/90 backdrop-blur rounded-lg shadow-md p-4 ">
       {user && (
-        <div className="flex items-center gap-3 ">
-          {user.photoURL && (
-            <img
-              src={user.photoURL}
-              alt={user.displayName ?? "User"}
-              className="w-10 h-10 rounded-full"
-            />
-          )}
-          <div className="flex justify-between w-full items-center">
-            <div>
-              <p className="text-sm font-semibold text-slate-800">
-                {user.displayName ?? "Usuário"}
-              </p>
-              <p className="text-xs text-slate-600">{user.email}</p>
+        <div className="flex flex-col gap-3 ">
+          <div>
+            <span className="text-xs  text-slate-600 line-clamp-2">
+              Bem-vindo {user.displayName ?? "usuário"} !
+            </span>
+          </div>
+          <hr className="border-t border-slate-300" />
+          <div className="flex items-center gap-3 ">
+            {user.photoURL && (
+              <img
+                src={user.photoURL}
+                alt={user.displayName ?? "User"}
+                className="w-10 h-10 rounded-full"
+              />
+            )}
+            <div className="flex justify-between w-full items-center">
+              <div>
+                <p className="text-sm font-semibold text-slate-800">
+                  {user.displayName ?? "Usuário"}
+                </p>
+                <p className="text-xs text-slate-600">{user.email}</p>
+              </div>
+              <FaPowerOff
+                color="#aeb3b8"
+                size={14}
+                className="mr-1 w-fit"
+                onClick={logout}
+              />
             </div>
-            <FaPowerOff
-              color="#aeb3b8"
-              size={14}
-              className="mr-1 w-fit"
-              onClick={logout}
-            />
           </div>
         </div>
       )}
