7adaf68e9fe34f78fe18c59f315d50511cc38f91
wakenedo <a_nuner@outlook.com>
2026-02-06 01:56:20 -0300

    chore(UserAreaInterface): Removed old comments

--- components/UserAreaInterface/UserAreaInterface.tsx
+++ components/UserAreaInterface/UserAreaInterface.tsx
@@ -14,18 +14,11 @@ const UserAreaInterface = ({
 }) => {
   return (
     <div className="relative min-h-screen font-sans overflow-hidden">
-      {/* Overlay */}
       <div className="absolute inset-0 bg-black/50 z-[-1]" />
-
       <main className="md:max-w-3xl mx-auto relative z-10 min-h-screen flex flex-col items-center pt-6 px-4">
-        {/* Promo */}
         <div className="w-full  mb-6">
           <BannerPromo />
         </div>
-
-        {/* User Card */}
-
-        {/* Stats */}
         {user && (
           <div className="w-full  grid gap-4 mb-6">
             <UserCard user={user} logout={logout} />
