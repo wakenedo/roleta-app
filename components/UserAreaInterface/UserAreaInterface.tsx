57411c65904d50c0ef7c6b81f13bef278ba6c506
wakenedo <a_nuner@outlook.com>
2026-02-10 02:27:56 -0300

    feat(UserAreaInterface): Removed BannerPromo

--- components/UserAreaInterface/UserAreaInterface.tsx
+++ components/UserAreaInterface/UserAreaInterface.tsx
@@ -18,10 +18,7 @@ const UserAreaInterface = ({
   return (
     <div className="relative min-h-screen font-sans overflow-hidden">
       <div className="absolute inset-0 bg-black/50 z-[-1]" />
-      <main className="md:max-w-3xl mx-auto relative z-10 min-h-screen flex flex-col items-center pt-6 px-4">
-        <div className="w-full  mb-6">
-          <BannerPromo />
-        </div>
+      <main className="mt-8 md:max-w-3xl mx-auto relative z-10 min-h-screen flex flex-col items-center pt-6 px-4">
         {user && (
           <div className="w-full  grid gap-4 mb-6">
             <UserCard user={user} logout={logout} />
