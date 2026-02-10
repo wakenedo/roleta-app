e757334816bd045727b8bb29f7be2e8fcf8ecf21
wakenedo <a_nuner@outlook.com>
2026-02-10 02:34:02 -0300

    feat(DailyOffersHistory): Updated structure

--- components/UserAreaInterface/DailyQuota/components/DailyOffersHistory/DailyOffersHistory.tsx
+++ components/UserAreaInterface/DailyQuota/components/DailyOffersHistory/DailyOffersHistory.tsx
@@ -1,18 +1,42 @@
 import { Product } from "@/components/Slots/types";
 import { UserAreaSectionBackground } from "@/components/UserAreaInterface/UserAreaSectionBackground";
+import { redirect } from "next/navigation";
 
 const DailyOffersHistory = ({
   historyPreview,
 }: {
-  historyPreview: unknown; // we'll talk about typing later
+  historyPreview: unknown;
 }) => {
   const offers = Array.isArray(historyPreview)
     ? historyPreview
     : [historyPreview];
 
-  const firstOfferProducts = offers[0]?.products ?? [];
+  const allProducts = offers.flatMap((offer) => offer?.products ?? []);
 
-  console.log("products from first offer", firstOfferProducts);
+  if (allProducts.length === 0) {
+    return (
+      <UserAreaSectionBackground>
+        <h3 className="text-sm font-semibold text-slate-800 mb-2">
+          Histórico de Ofertas
+        </h3>
+        <hr className="border-t border-slate-300 mb-4" />
+        <div className="text-center mb-2 space-y-2 flex flex-col max-w-sm mx-auto">
+          <div className="my-4">
+            <span>Sem histórico por enquanto...</span>
+          </div>
+          <button
+            className="
+      cursor-pointer  py-2 drop-shadow-xl text-shadow-2xs
+       bg-green-400 hover:bg-green-200 transition
+      text-slate-50 rounded-xs disabled:bg-slate-400 pb-2 font-semibold"
+            onClick={() => redirect("/")}
+          >
+            Começe a Jogar
+          </button>
+        </div>
+      </UserAreaSectionBackground>
+    );
+  }
 
   return (
     <UserAreaSectionBackground>
@@ -20,9 +44,9 @@ const DailyOffersHistory = ({
         Histórico de Ofertas
       </h3>
       <hr className="border-t border-slate-300 mb-4" />
-      {!historyPreview && <div>Começe a girar</div>}
+
       <div>
-        {firstOfferProducts.map((product: Product) => (
+        {allProducts.map((product: Product) => (
           <div key={product.id}>{product.name}</div>
         ))}
       </div>
