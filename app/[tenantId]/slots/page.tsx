27aa6645a939e5a617f8e2fdab02906a6158fbf5
wakenedo <a_nuner@outlook.com>
2026-02-26 00:36:20 -0300

    feat([tenantId]/slots): Using TenantSlotsDedicatedRouteBackground for more brand exposure while on tenant experience

--- app/[tenantId]/slots/page.tsx
+++ app/[tenantId]/slots/page.tsx
@@ -1,31 +1,36 @@
 "use client";
 import { Slots } from "@/components/Slots";
+import { TenantSlotsDedicatedRouteBackground } from "@/components/TenantSlotsDedicatedRouteBackground";
 import { useTenant } from "@/context/TenantContext/TenantContext";
 import { useParams } from "next/navigation";
 
 export default function TenantSlotsPage() {
-  const { tenant, loading, error, tenantQuota } = useTenant();
+  const { tenant, loading, error } = useTenant();
   const { tenantId } = useParams();
   if (!tenant) return;
   if (tenantId != tenant.id) return;
   const tenantName = tenant.name;
   const tenantSettings = tenant.settings;
   const tenantBranding = tenant.branding;
+  const primaryColor =
+    tenantBranding && (tenantBranding.primaryColor as string);
   if (!tenantSettings && !tenantBranding) return;
   const paramTenantId = tenantId;
 
-  console.log("tenant", tenantId);
+  console.log("tenantSettings", tenantSettings);
+  console.log("primaryColor", primaryColor);
 
-  console.log("tenant quota", tenantQuota);
   return (
-    <>
-      <div>{tenant.id}</div>
+    <TenantSlotsDedicatedRouteBackground
+      tenantBranding={tenantBranding}
+      tenantName={tenantName}
+    >
       <Slots
         tenantId={paramTenantId}
         tenantBranding={tenantBranding}
         tenantSettings={tenantSettings}
         tenantName={tenantName}
       />
-    </>
+    </TenantSlotsDedicatedRouteBackground>
   );
 }
