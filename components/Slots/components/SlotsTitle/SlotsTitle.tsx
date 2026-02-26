ec93113dc17d9c484abb33eb4dbde7c1820f570f
wakenedo <a_nuner@outlook.com>
2026-02-26 00:38:58 -0300

    feat(SlotsTitle): Added primaryColor to tenantNAme on SlotsTitle for tenant experience

--- components/Slots/components/SlotsTitle/SlotsTitle.tsx
+++ components/Slots/components/SlotsTitle/SlotsTitle.tsx
@@ -1,10 +1,24 @@
+import { TenantBranding } from "@/context/TenantContext/types";
 import { SlotsTitleGradient } from "./components/SlotsTitleGradient";
 
-const SlotsTitle = ({ tenantName }: { tenantName?: string }) => {
+const SlotsTitle = ({
+  tenantName,
+  tenantBranding,
+}: {
+  tenantName?: string;
+  tenantBranding?: TenantBranding;
+}) => {
+  const primaryColor =
+    tenantBranding && (tenantBranding.primaryColor as string);
   return (
     <>
       {tenantName && (
-        <div className="ml-5 md:text-2xl font-semibold">{tenantName}</div>
+        <div
+          style={{ "--primary": primaryColor } as React.CSSProperties}
+          className={`ml-5 md:text-4xl font-extrabold text-[var(--primary)] [-webkit-text-stroke:1px_#fdfd] drop-shadow-[1px_2px_5px_rgba(255,215,0,0.8)]`}
+        >
+          {tenantName}
+        </div>
       )}
       <SlotsTitleGradient />
     </>
