94f6599a0196a4fecc9384520a16673021d40691
wakenedo <a_nuner@outlook.com>
2026-03-01 00:29:51 -0300

    styles(Tenant & UserSubscriptionModes): Add bg and new color for fonts

--- components/HomePageInterface/components/TenantSubscriptionModes/TenantSubscriptionModes.tsx
+++ components/HomePageInterface/components/TenantSubscriptionModes/TenantSubscriptionModes.tsx
@@ -2,12 +2,12 @@ const TenantSubscriptionModes = () => {
   return (
     <div
       id="subscription-modes-tenant"
-      className="border-2 flex flex-col border-black md:mx-2 h-100"
+      className="border-2 flex flex-col border-black bg-white md:mx-2 h-100"
     >
       <div>
-        <span className="text-3xl">Tenant Subscription Modes</span>
+        <span className="text-3xl text-black">Tenant Subscription Modes</span>
       </div>
-      <div>
+      <div className="text-black">
         This is early tenant Subscription Modes: Sub Cards and explanation
         interface for both iterations of Home Page ill be removed or swapped
         with another interface if tenant user is max sub,if not we can use this
