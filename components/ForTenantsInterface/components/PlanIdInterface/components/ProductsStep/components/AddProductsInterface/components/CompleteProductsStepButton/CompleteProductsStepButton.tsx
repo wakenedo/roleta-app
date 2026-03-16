fa16f02385f3b5ae0cab9f75bfecfc1a44895d9c
wakenedo <a_nuner@outlook.com>
2026-03-16 03:50:20 -0300

    feat(CompleteProductsStepButton): Initial validation for complete button

--- components/ForTenantsInterface/components/PlanIdInterface/components/ProductsStep/components/AddProductsInterface/components/CompleteProductsStepButton/CompleteProductsStepButton.tsx
+++ components/ForTenantsInterface/components/PlanIdInterface/components/ProductsStep/components/AddProductsInterface/components/CompleteProductsStepButton/CompleteProductsStepButton.tsx
@@ -3,16 +3,18 @@ import { TenantProduct } from "@/context/TenantContext/types";
 const CompleteProductsStepButton = ({
   products,
   onSave,
+  areProductsValidated,
 }: {
   products: TenantProduct[];
   onSave: (products: TenantProduct[]) => void;
+  areProductsValidated: boolean;
 }) => {
   return (
     <button
       disabled={products.length === 0}
       onClick={() => onSave(products)}
       className={`py-3 rounded-lg ${
-        products.length === 0
+        areProductsValidated != true
           ? "bg-gray-300 cursor-not-allowed"
           : "bg-emerald-400"
       }`}
