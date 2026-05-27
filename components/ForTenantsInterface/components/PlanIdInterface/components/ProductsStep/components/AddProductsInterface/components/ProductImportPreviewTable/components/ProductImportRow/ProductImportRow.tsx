5148c2b3b091a58622832cd1011f3e33380a905e
wakenedo <a_nuner@outlook.com>
2026-05-27 17:02:59 -0300

    feat(ProductImportRow): Using new data from API to display in catalog manager more relevant information about tenant catalog and products

--- components/ForTenantsInterface/components/PlanIdInterface/components/ProductsStep/components/AddProductsInterface/components/ProductImportPreviewTable/components/ProductImportRow/ProductImportRow.tsx
+++ components/ForTenantsInterface/components/PlanIdInterface/components/ProductsStep/components/AddProductsInterface/components/ProductImportPreviewTable/components/ProductImportRow/ProductImportRow.tsx
@@ -9,8 +9,15 @@ const ProductImportRow = ({
   const missing = (value: TenantProduct[keyof TenantProduct]) =>
     !value ? "border-red-400" : "border-gray-300";
 
+  // For now we're allowing the user to edit all fields during import,
+  // but we might want to lock some of them in the future, if not all of them.
+  // Ideally we develop a simple structure to verify the integrity of the imported data,
+  // in the catalog management interface, to verify link integrity and data integrity.
+  // To minimize the tenant user input errors and improper data formatting,
+  // ensure a smooth import process and product management for the tenants.
+
   return (
-    <div className="grid grid-cols-5 gap-2 p-2 border-b">
+    <div className="grid grid-cols-7 gap-2 p-2 border-b">
       <input
         className={`border p-1 text-xs ${missing(product.name)}`}
         value={product.name}
@@ -19,7 +26,7 @@ const ProductImportRow = ({
 
       <input
         className={`border p-1 text-xs ${missing(product.image)}`}
-        value={product.image}
+        value={product.image || undefined}
         onChange={(e) => updateProductField(index, "image", e.target.value)}
       />
 
@@ -28,6 +35,15 @@ const ProductImportRow = ({
         value={product.url}
         onChange={(e) => updateProductField(index, "url", e.target.value)}
       />
+      {product.offerUrl !== undefined && product.offerUrl != product.url && (
+        <input
+          className={`border p-1 text-xs ${missing(product.offerUrl)}`}
+          value={product.offerUrl}
+          onChange={(e) =>
+            updateProductField(index, "offerUrl", e.target.value)
+          }
+        />
+      )}
 
       <select
         className="border text-xs"
@@ -40,11 +56,28 @@ const ProductImportRow = ({
       </select>
 
       <input
+        type="string"
+        className="border text-xs"
+        value={product.commissionRate ?? ""}
+        onChange={(e) =>
+          updateProductField(index, "commissionRate", e.target.value)
+        }
+      />
+
+      <input
         type="number"
         className="border text-xs"
         value={product.price ?? ""}
         onChange={(e) =>
           updateProductField(index, "price", Number(e.target.value))
+        }
+      />
+      <input
+        type="number"
+        className="border text-xs"
+        value={product.commission ?? ""}
+        onChange={(e) =>
+          updateProductField(index, "commission", Number(e.target.value))
         }
       />
     </div>
