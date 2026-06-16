5a99c290a7181a74671ec5aff585a3142ffa5976
wakenedo <a_nuner@outlook.com>
2026-06-16 18:20:37 -0300

    feat(AllTimeAnalytics & SeasonalAnalytics): Using the new component to relative components

--- components/TenantAreaInterface/components/TenantCard/components/TenantOptionsAnalytics/components/SeasonalAnalytics/SeasonalAnalytics.tsx
+++ components/TenantAreaInterface/components/TenantCard/components/TenantOptionsAnalytics/components/SeasonalAnalytics/SeasonalAnalytics.tsx
@@ -1,42 +1,44 @@
-import { SeasonPlatformStats } from "@/hooks/useTenantSeasonStats";
 import { InfoRow } from "../../../TenantGeneralInterface/components/InfoRow";
+import { SeasonStatsProps } from "@/context/TenantContext/types";
+import { DynamicStatsInterface } from "../DynamicStatsInterface";
 
 const SeasonalAnalytics = ({
   seasonStats,
   loading,
 }: {
-  seasonStats: SeasonPlatformStats;
+  seasonStats: SeasonStatsProps;
   loading: boolean;
 }) => {
   console.log("SeasonalAnalytics seasonStats", seasonStats);
   return (
-    <div className="flex flex-col justify-between   px-1 mx-1 mt-2 md:max-h-160">
-      {loading ? (
-        <p>Carregando...</p>
-      ) : (
-        <div className="flex flex-col justify-between    md:max-h-160 ">
-          <div className="border border-slate-200 bg-slate-200 p-4 mt-2 md:h-dvh">
-            Dynamic Component
-          </div>
+    <div className="flex flex-col justify-between   px-1  md:max-h-160">
+      {seasonStats ? (
+        <>
+          <DynamicStatsInterface
+            engagementStats={seasonStats.stats.engagement}
+            mode="season"
+          />
           <div className="mt-2">
             <InfoRow
               label="Giros"
-              value={seasonStats.stats?.platform.totalSpins ?? "N/A"}
+              value={seasonStats.stats.platform.totalSpins ?? "N/A"}
             />
             <InfoRow
               label="Novos Usuários"
-              value={seasonStats.stats?.platform.totalUsers ?? "N/A"}
+              value={seasonStats.stats.platform.totalUsers ?? "N/A"}
             />
             <InfoRow
               label="Recompensas Exibidas"
-              value={seasonStats.stats?.platform.totalRewardsShown ?? "N/A"}
+              value={seasonStats.stats.platform.totalRewardsShown ?? "N/A"}
             />
             <InfoRow
               label="Pontuação do Ranking"
               value={seasonStats.ranking?.score ?? "N/A"}
             />
           </div>
-        </div>
+        </>
+      ) : (
+        <p className="mt-2">No analytics data available.</p>
       )}
     </div>
   );
