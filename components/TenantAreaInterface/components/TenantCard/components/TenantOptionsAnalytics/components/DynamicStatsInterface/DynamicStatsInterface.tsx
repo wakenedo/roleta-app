029f48d78e6e8521aa2858defec882ffccc03f64
wakenedo <a_nuner@outlook.com>
2026-06-16 18:18:26 -0300

    feat(TenantOptionsAnalytics/DynamicStatsInterface): Add component to orchestrate the engagement metrics for tenants for both modes

---
+++ components/TenantAreaInterface/components/TenantCard/components/TenantOptionsAnalytics/components/DynamicStatsInterface/DynamicStatsInterface.tsx
@@ -0,0 +1,111 @@
+import { TenantSectionMarker } from "@/components/TenantAreaInterface/components/TenantSectionMarker";
+import { DynamicStatsInterfaceProps } from "@/context/TenantContext/types";
+
+const DynamicStatsInterface = ({
+  engagementStats,
+  mode = "global",
+}: DynamicStatsInterfaceProps) => {
+  return (
+    <div className="border border-slate-200 bg-slate-200 p-1 mt-2 rounded drop-shadow">
+      <div className="border border-slate-50 rounded p-2">
+        <div className=" space-y-1 text-slate-600 cursor-default px-1 text-md">
+          <div className=" flex justify-between font-semibold tracking-widest">
+            <span>Click Events</span> {engagementStats.clickEvents}
+          </div>
+
+          <div className=" flex justify-between font-semibold tracking-widest">
+            <span>Produtos Clicados</span> {engagementStats.clickedProducts}
+          </div>
+
+          <div className=" flex justify-between font-semibold tracking-widest">
+            <span>Média de Clicks por Produto</span>{" "}
+            <span className="text-base">
+              {engagementStats.averageClicksPerProduct}
+            </span>
+          </div>
+        </div>
+
+        {/* Top Categories */}
+        <div className="mt-3 ">
+          <TenantSectionMarker markerTitle="Top Categorias" />
+
+          {engagementStats.topCategories?.length ? (
+            <ul className="text-sm mt-2 text-slate-600 cursor-default">
+              {engagementStats.topCategories.map((category) => (
+                <li
+                  key={category.name}
+                  className="flex justify-between tracking-widest capitalize text-base items-center"
+                >
+                  <span>{category.name}</span>
+                  <span className="font-semibold">
+                    {category.clicks} cliques
+                  </span>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p className="text-sm text-slate-500">
+              Nenhuma categoria encontrada
+            </p>
+          )}
+        </div>
+
+        {/* Top Products */}
+        <div className="mt-3 ">
+          <TenantSectionMarker markerTitle="Top Produtos" />
+
+          {engagementStats.topProducts?.length ? (
+            <ul className="text-sm mt-2 text-slate-600 cursor-default">
+              {engagementStats.topProducts.slice(0, 5).map((product, index) => (
+                <li
+                  key={`${product.url}-${index}`}
+                  className="flex justify-between tracking items-center"
+                >
+                  <span className="text-xs my-1">{product.name}</span>
+                  <span className="tracking-widest text-base capitalize font-semibold">
+                    {product.clicks} cliques
+                  </span>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p className="text-sm text-slate-500">Nenhum produto encontrado</p>
+          )}
+        </div>
+
+        {/* Affiliate Providers */}
+        <div className="mt-3  ">
+          <TenantSectionMarker markerTitle="Provedores Afiliados" />
+
+          {engagementStats.affiliateProviders?.length ? (
+            <ul className="text-sm mt-2 text-slate-600 cursor-default">
+              {engagementStats.affiliateProviders.map((provider) => (
+                <li
+                  key={provider.name}
+                  className="flex justify-between capitalize"
+                >
+                  <span className="tracking-widest text-base">
+                    {provider.name}
+                  </span>
+                  <span className="tracking-widest  text-base font-semibold">
+                    {provider.clicks} cliques
+                  </span>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p className="text-sm text-slate-500">Nenhum provedor encontrado</p>
+          )}
+        </div>
+
+        {/* Footer */}
+        <div className="mt-3 border-t border-slate-50 pt-2 text-xs text-slate-500">
+          Última atualização:{" "}
+          {new Date(engagementStats.lastUpdatedAt).toLocaleString("pt-BR")}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DynamicStatsInterface;
---
+++ components/TenantAreaInterface/components/TenantCard/components/TenantOptionsAnalytics/components/DynamicStatsInterface/index.ts
@@ -0,0 +1,1 @@
+export { default as DynamicStatsInterface } from "./DynamicStatsInterface";
