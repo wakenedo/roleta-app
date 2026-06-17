b85d03489dbe99ed4a8a69655f72b6dd2de8e1c2
wakenedo <a_nuner@outlook.com>
2026-06-17 06:48:12 -0300

    styles(ProductGrid): h-100 > lg:h-[695px]

--- components/TenantAreaInterface/components/TenantProductCatalog/components/ProductGrid/ProductGrid.tsx
+++ components/TenantAreaInterface/components/TenantProductCatalog/components/ProductGrid/ProductGrid.tsx
@@ -8,7 +8,7 @@ const ProductGrid = ({ products }: TenantProductCatalogProductGridProps) => {
     <div className="flex flex-col  ">
       <TenantSectionMarker markerTitle="Produtos Carregados" />
 
-      <div className="border-b border-x border-slate-200 p-2 grid grid-cols-2 md:grid-cols-3  gap-4 h-100 mb-2 overflow-scroll   [scrollbar-width:none] bg-slate-100">
+      <div className="border-b border-x border-slate-200 p-2 grid grid-cols-2 md:grid-cols-3  gap-4 lg:h-[695px]  overflow-scroll   [scrollbar-width:none] bg-slate-100">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
