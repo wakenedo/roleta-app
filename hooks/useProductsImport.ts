078042d12b47ed1d28258859d261ff767f95bc0c
wakenedo <a_nuner@outlook.com>
2026-07-02 12:30:44 -0300

    feat(useProductsImport): Extended utils methods and using on hook file

--- hooks/useProductsImport.ts
+++ hooks/useProductsImport.ts
@@ -3,11 +3,17 @@
 import { useState } from "react";
 import { TenantProduct } from "@/context/TenantContext/types";
 import {
+  _handleFileUpload,
+  _validateProducts,
   getPaginationMeta,
-  normalizeProducts,
   paginateProducts,
+  PRODUCTS_PER_PAGE,
 } from "./utils/productImportsHelpers";
-import { RawProductsProps, UseProductsImportsProps } from "./types";
+import {
+  CsvPreviewProps,
+  JsonPreviewProps,
+  UseProductsImportsProps,
+} from "./types";
 import { selectedPlanMaxProducts } from "@/Interfaces/ForTenantsInterface/components/PlanIdInterface/utils";
 
 export const useProductsImport = ({
@@ -16,28 +22,15 @@ export const useProductsImport = ({
   importProductsJSON,
 }: UseProductsImportsProps) => {
   const [file, setFile] = useState<File | null>(null);
-  const [csvPreview, setCsvPreview] = useState<{
-    preview: unknown[];
-    errors: string[];
-    total: number;
-    valid: number;
-  } | null>(null);
-  const [jsonPreview, setJsonPreview] = useState<{
-    preview: unknown[];
-    products: unknown[];
-    errors: string[];
-    total: number;
-    valid: number;
-  } | null>(null);
+  const [csvPreview, setCsvPreview] = useState<CsvPreviewProps>(null);
+  const [jsonPreview, setJsonPreview] = useState<JsonPreviewProps>(null);
   const [fileName, setFileName] = useState<string | null>(null);
-  const [rawProducts, setRawProducts] = useState<RawProductsProps[]>([]);
   const [products, setProducts] = useState<TenantProduct[]>([]);
   const [errors, setErrors] = useState<string[]>([]);
   const [isValidated, setIsValidated] = useState(false);
   const [page, setPage] = useState(1);
 
   const MAX_PRODUCTS = selectedPlanMaxProducts(selectedPlan);
-  const PRODUCTS_PER_PAGE = 20;
 
   const paginatedProducts = paginateProducts(products, page, PRODUCTS_PER_PAGE);
 
@@ -47,62 +40,28 @@ export const useProductsImport = ({
     page,
   );
 
-  const validateProducts = () => {
-    const newErrors: string[] = [];
-    products.forEach((p, index) => {
-      if (!p.name) newErrors.push(`Product ${index + 1} missing name`);
-      if (!p.url) newErrors.push(`Product ${index + 1} missing url`);
-      if (p.price !== null && p.price != undefined && p.price < 0)
-        newErrors.push(`Product ${index + 1} invalid price`);
-    });
+  const validateProducts = _validateProducts({
+    products,
+    setErrors,
+    setIsValidated,
+  });
 
-    if (newErrors.length > 0) {
-      setErrors(newErrors);
-      setIsValidated(false);
-      return false;
-    }
-
-    setIsValidated(true);
-    return true;
-  };
-
-  const handleFileUpload = async (file: File) => {
-    setErrors([]);
-    setFileName(file.name);
-    setFile(file);
-
-    if (file.name.endsWith(".json") && importProductsJSON) {
-      const jPreview = (await importProductsJSON(file, true)) as {
-        total: number;
-        valid: number;
-        preview: unknown[];
-        products: unknown[];
-        errors: string[];
-      };
-      if (jPreview.products.length > MAX_PRODUCTS) {
-        setErrors([`Plan allows only ${MAX_PRODUCTS} products`]);
-        return;
-      }
-      const productsArray = jPreview.products as TenantProduct[];
-
-      setProducts(productsArray);
-      setJsonPreview(jPreview);
-    }
-    if (file.name.endsWith(".csv") && importProductsCSV) {
-      const cPreview = (await importProductsCSV(file, true)) as {
-        preview: unknown[];
-        errors: string[];
-        total: number;
-        valid: number;
-      };
-      setCsvPreview(cPreview);
-    }
-  };
+  const handleFileUpload = _handleFileUpload({
+    file: file,
+    importProductsCSV,
+    importProductsJSON,
+    maxProducts: MAX_PRODUCTS,
+    setCsvPreview,
+    setJsonPreview,
+    setFile,
+    setProducts,
+    setFileName,
+    setErrors,
+  });
 
   return {
     file,
     fileName,
-    rawProducts,
     jsonPreview,
     csvPreview,
     setJsonPreview,
--- hooks/utils/productImportsHelpers.ts
+++ hooks/utils/productImportsHelpers.ts
@@ -1,4 +1,6 @@
 import { TenantProduct } from "@/context/TenantContext/types";
+import { SetStateAction } from "react";
+import { CsvPreviewProps, JsonPreviewProps } from "../types";
 
 /*
 FIELD CANDIDATES
@@ -9,6 +11,7 @@ const NAME_FIELDS = ["name", "title", "productName"];
 const IMAGE_FIELDS = ["image", "image_url", "thumbnail", "picture"];
 const URL_FIELDS = ["url", "link", "product_url", "affiliate_link"];
 const PRICE_FIELDS = ["price", "amount", "value"];
+const PRODUCTS_PER_PAGE = 20;
 
 /*
 Utility: find first existing field
@@ -111,7 +114,7 @@ const normalizeProduct = (p: Record<string, unknown>): TenantProduct => {
 Normalize feed
 */
 
-export const normalizeProducts = (items: unknown[]): TenantProduct[] => {
+const normalizeProducts = (items: unknown[]): TenantProduct[] => {
   return items
     .filter(
       (p): p is Record<string, unknown> => typeof p === "object" && p !== null,
@@ -119,18 +122,14 @@ export const normalizeProducts = (items: unknown[]): TenantProduct[] => {
     .map((p) => normalizeProduct(p));
 };
 
-export const paginateProducts = <T>(
-  items: T[],
-  page: number,
-  perPage: number,
-) => {
+const paginateProducts = <T>(items: T[], page: number, perPage: number) => {
   const start = (page - 1) * perPage;
   const end = start + perPage;
 
   return items.slice(start, end);
 };
 
-export const getPaginationMeta = (
+const getPaginationMeta = (
   totalItems: number,
   perPage: number,
   currentPage: number,
@@ -145,4 +144,100 @@ export const getPaginationMeta = (
     hasNext: currentPage < totalPages,
     hasPrev: currentPage > 1,
   };
+};
+
+const _validateProducts = ({
+  products,
+  setErrors,
+  setIsValidated,
+}: {
+  products: TenantProduct[];
+  setErrors: (value: SetStateAction<string[]>) => void;
+  setIsValidated: (value: SetStateAction<boolean>) => void;
+}) => {
+  const newErrors: string[] = [];
+  products.forEach((p, index) => {
+    if (!p.name) newErrors.push(`Product ${index + 1} missing name`);
+    if (!p.url) newErrors.push(`Product ${index + 1} missing url`);
+    if (p.price !== null && p.price != undefined && p.price < 0)
+      newErrors.push(`Product ${index + 1} invalid price`);
+  });
+
+  if (newErrors.length > 0) {
+    setErrors(newErrors);
+    setIsValidated(false);
+    return false;
+  }
+
+  setIsValidated(true);
+  return true;
+};
+
+const _handleFileUpload = async ({
+  file,
+  setErrors,
+  setFileName,
+  setFile,
+  setJsonPreview,
+  setCsvPreview,
+  setProducts,
+  importProductsJSON,
+  importProductsCSV,
+  maxProducts,
+}: {
+  file: File | null;
+  setErrors: (value: SetStateAction<string[]>) => void;
+  setFileName: (value: SetStateAction<string | null>) => void;
+  setFile: (value: SetStateAction<File | null>) => void;
+  setProducts: (value: SetStateAction<TenantProduct[]>) => void;
+  setJsonPreview: (value: SetStateAction<JsonPreviewProps>) => void;
+  setCsvPreview: (value: SetStateAction<CsvPreviewProps>) => void;
+  importProductsJSON:
+    | ((file: File, dryRun?: boolean) => Promise<unknown>)
+    | undefined;
+  importProductsCSV:
+    | ((file: File, dryRun?: boolean) => Promise<unknown>)
+    | undefined;
+  maxProducts: 100 | 200 | 500 | 50;
+}) => {
+  if (!file) return;
+  setErrors([]);
+  setFileName(file.name);
+  setFile(file);
+
+  if (file.name.endsWith(".json") && importProductsJSON) {
+    const jPreview = (await importProductsJSON(file, true)) as {
+      total: number;
+      valid: number;
+      preview: unknown[];
+      products: unknown[];
+      errors: string[];
+    };
+    if (jPreview.products.length > maxProducts) {
+      setErrors([`Plan allows only ${maxProducts} products`]);
+      return;
+    }
+    const productsArray = jPreview.products as TenantProduct[];
+
+    setProducts(productsArray);
+    setJsonPreview(jPreview);
+  }
+  if (file.name.endsWith(".csv") && importProductsCSV) {
+    const cPreview = (await importProductsCSV(file, true)) as {
+      preview: unknown[];
+      errors: string[];
+      total: number;
+      valid: number;
+    };
+    setCsvPreview(cPreview);
+  }
+};
+
+export {
+  getPaginationMeta,
+  paginateProducts,
+  normalizeProducts,
+  PRODUCTS_PER_PAGE,
+  _validateProducts,
+  _handleFileUpload,
 };
